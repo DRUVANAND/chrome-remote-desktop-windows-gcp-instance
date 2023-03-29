91d97683c992002b071aab48148c294535e6a2fd
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-29 21:21:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,51 @@
-/*resource "google_compute_instance" "chrome_desktop" {
-  name         = "automate-windows-chrome-remote-desktop"
-  machine_type = "e2-medium"
+resource "google_compute_instance" "chrome-remote-desktop" {
+  name         = "chrome-remote-desktop"
+  machine_type = "n1-standard-2"
+  zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "windows-server-2022-dc-v20230315"
+      image = "windows-cloud/windows-2019"
     }
   }
 
   network_interface {
     network = "default"
-
-    access_config {
-      // Optional. External IP address configuration.
-    }
   }
 
- metadata_startup_script = "powershell.exe Set-ExecutionPolicy RemoteSigned -force; (new-object System.Net.WebClient).DownloadFile('https://dl.google.com/chrome-remote-desktop/chromeremotedesktophost.msi', 'C:/chromeremotedesktophost.msi'); Start-Process 'msiexec.exe' -Wait -ArgumentList '/i C:/chromeremotedesktophost.msi /quiet /qn /norestart'; Remove-Item C:/chromeremotedesktophost.msi -Force"
+  metadata_startup_script = "${file("chrome-remote-desktop.ps1")}"
 
- tags = ["chrome-remote-desktop"]
+  metadata = {
+    windows-startup-script-ps1 = "${file("install-chrome-remote-desktop.ps1")}"
+  }
+
+  tags = ["chrome-remote-desktop"]
 }
 
+resource "google_compute_firewall" "chrome-remote-desktop" {
+  name        = "chrome-remote-desktop"
+  network     = "default"
+  priority    = 1000
+  direction   = "INGRESS"
+  action      = "ALLOW"
+  source_tags = ["chrome-remote-desktop"]
 
-resource "google_compute_firewall" "chrome_desktop" {
-  name    = "chrome-desktop"
-  network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["443","3389"]
+    ports    = ["3389", "5222", "8889"]
   }
-  source_ranges = ["0.0.0.0/0"]
 }
-*/
+
+resource "google_compute_firewall" "chrome-remote-desktop-rdp" {
+  name        = "chrome-remote-desktop-rdp"
+  network     = "default"
+  priority    = 1001
+  direction   = "INGRESS"
+  action      = "ALLOW"
+  source_tags = ["chrome-remote-desktop"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+}
