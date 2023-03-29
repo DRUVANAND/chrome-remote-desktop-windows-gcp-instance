de78e3ee1337189cb822a69264c0e8952180a392
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-29 21:22:36 +0530

    Create install-chrome-remote-desktop.ps1

---
+++ install-chrome-remote-desktop.ps1
@@ -0,0 +1,4 @@
+# Download and install the Chrome Remote Desktop Host
+(new-object System.Net.WebClient).DownloadFile('https://dl.google.com/chrome-remote-desktop/chromeremotedesktophost.msi', 'C:/chromeremotedesktophost.msi')
+Start-Process 'msiexec.exe' -Wait -ArgumentList '/i C:/chromeremotedesktophost.msi /quiet /qn /norestart'
+Remove-Item C:/chromeremotedesktophost.msi -Force
