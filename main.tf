25b9fcfd275686c494178e7ef9cf74ba91779bd8
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-29 21:26:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,29 +23,15 @@ resource "google_compute_instance" "chrome-remote-desktop" {
 }
 
 resource "google_compute_firewall" "chrome-remote-desktop" {
-  name        = "chrome-remote-desktop"
-  network     = "default"
-  priority    = 1000
-  direction   = "INGRESS"
-  action      = "ALLOW"
-  source_tags = ["chrome-remote-desktop"]
+  name    = "allow-chrome-remote-desktop"
+  network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["3389", "5222", "8889"]
+    ports    = ["3389", "22", "5222", "8889"]
   }
-}
 
-resource "google_compute_firewall" "chrome-remote-desktop-rdp" {
-  name        = "chrome-remote-desktop-rdp"
-  network     = "default"
-  priority    = 1001
-  direction   = "INGRESS"
-  action      = "ALLOW"
-  source_tags = ["chrome-remote-desktop"]
-
-  allow {
-    protocol = "tcp"
-    ports    = ["3389"]
-  }
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+  target_tags   = ["chrome-remote-desktop"]
 }
