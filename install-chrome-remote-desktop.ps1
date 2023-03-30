d2fecea53438a87abd22d515e8ca1d222f8a677c
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-30 13:49:52 +0530

    Delete install-chrome-remote-desktop.ps1

--- install-chrome-remote-desktop.ps1
+++
@@ -1,4 +0,0 @@
-# Download and install the Chrome Remote Desktop Host
-(new-object System.Net.WebClient).DownloadFile('https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi', 'C:/chromeremotedesktophost.msi')
-Start-Process 'msiexec.exe' -Wait -ArgumentList '/i C:/chromeremotedesktophost.msi /quiet /qn /norestart'
-Remove-Item C:/chromeremotedesktophost.msi -Force
