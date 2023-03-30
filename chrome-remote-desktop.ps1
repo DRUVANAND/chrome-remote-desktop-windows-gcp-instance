cd7ea02801291e2d5478fcb988c1f322bfb898eb
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-30 13:49:41 +0530

    Delete chrome-remote-desktop.ps1

--- chrome-remote-desktop.ps1
+++
@@ -1,8 +0,0 @@
-# Set execution policy for PowerShell
-Set-ExecutionPolicy RemoteSigned -force
-
-# Wait for 60 seconds to allow metadata server to fetch install script
-Start-Sleep -Seconds 60
-
-# Run the install script
-C:\install-chrome-remote-desktop.ps1
