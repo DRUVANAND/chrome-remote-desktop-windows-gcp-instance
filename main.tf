1eef0165428f61cc433ec006aeff787ac267117f
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-03-31 23:23:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_instance" "chrome-remote-desktop" {
+/*resource "google_compute_instance" "chrome-remote-desktop" {
   name         = "chrome-remote-desktop"
   machine_type = "n1-standard-2"
   zone         = "us-central1-a"
@@ -51,4 +51,4 @@ resource "google_compute_resource_policy" "hourly" {
     
   }
 }
-
+*/
