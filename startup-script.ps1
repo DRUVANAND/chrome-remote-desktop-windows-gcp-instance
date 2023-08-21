a0c4d981e4a25166260f0b5455d5419aca74a40b
GitHub-GCP <128677270+Gavin-GCP@users.noreply.github.com>
2023-08-21 14:31:48 +0530

    Delete startup-script.ps1

--- startup-script.ps1
+++
@@ -1,5 +0,0 @@
-  $installer = "$env:TEMP\chromeremotedesktophost.msi"
-  $uri = 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi'
-  (New-Object Net.WebClient).DownloadFile($uri,"$installer") 
-    Start-Process $installer -Wait 
-    Remove-Item $installer
